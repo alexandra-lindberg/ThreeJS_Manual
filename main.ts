475001bb2e9b1355853a474578559c3b675f878e
Alexandra <Lindberg_Alexandra@hotmail.com>
2024-06-23 20:04:42 +0200

    📱 Screen size adaption, WebGL support check
    Update adds callback function for changing screen size, as well as a check for whether or not the browser supports WebGL.

--- main.ts
+++ main.ts
@@ -1,27 +1,65 @@
 import './resources/stylesheets/style.css'
 import * as meshCollection from './resources/models/meshCollections.ts' 
-import * as three from 'three'; 
+import * as THREE from 'three';
 
-// The Big Three.
-const scene = new three.Scene();
-const camera = new three.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 1000);
-const renderer = new three.WebGLRenderer();
+import WebGL from 'three/addons/capabilities/WebGL.js';
 
-// Sets the size and appends the canvas element.
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+if ( WebGL.isWebGLAvailable() ) {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer( {
+        canvas: document.querySelector('#app') as HTMLElement,
+});
+    // Sets the size and appends the canvas element.
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+    
+    // Adds a callback function.
+    window.addEventListener('resize', callbackResizePage, false);
 
-// Draws scene into camera and renders.
-renderer.render( scene, camera);
+    // Sets function that is animation loop.
+    renderer.setAnimationLoop(animate);
+    
+    // Draws scene into camera and renders.
+    renderer.render( scene, camera);
+    
+    // Sets camera position
+    camera.position.set(0, 1, 5);
+    camera.rotateX(-0.2);
+    
+    // Renders blank canvas
+    renderer.render( scene, camera);
+    
+    // Adds objects
+    scene.add(meshCollection.cube);
+    
+    // Render loop.
+    function animate() {
+        meshCollection.cube.rotation.x += 0.01;
+        meshCollection.cube.rotation.y += 0.01;
+    
+      
+        renderer.render( scene, camera );
+    }
 
-camera.position.set(0, 1, 5);
-camera.rotateX(-0.2);
 
-scene.add(meshCollection.cube);
-renderer.render( scene, camera);
 
-// Render loop.
-function animate() {
-  
 
+
+
+    
+    
+    function callbackResizePage() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+} else {
+	const warning = WebGL.getWebGLErrorMessage();
+	const canvasElement = document.getElementById( "container" ); 
+
+    if(canvasElement === null) { console.error("ERROR: HTMLElement possibly null."); }
+    else { canvasElement.appendChild( warning );  }    
 }
+
+
--- resources/models/meshCollections.ts
+++ resources/models/meshCollections.ts
@@ -1,6 +1,6 @@
-import * as three from 'three'; 
+import * as THREE from 'three'; 
 
-const geometry = new three.BoxGeometry( 1, 1, 1 );
-const material = new three.MeshBasicMaterial( { color: 0x00ff00 } );
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 
-export const cube = new three.Mesh( geometry, material );
+export const cube = new THREE.Mesh( geometry, material );
